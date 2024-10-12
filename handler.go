d4e4446cfce116c6eab052cd980f3d73554ed840
VVatchful <tdbt1122@gmail.com>
2024-10-11 22:56:51 -0400

    project

---
+++ handler.go
@@ -0,0 +1,369 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+    "strings"
+	"time"
+    "strconv"
+
+	"gator/internal/database"
+
+	"github.com/google/uuid"
+)
+
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %v <name>", cmd.Name)
+	}
+
+	name := cmd.Args[0]
+
+	// Query the database to check if a user with the same name exists
+	_, err := s.db.GetUser(context.Background(), name)
+	if err != nil && err != sql.ErrNoRows {
+		// If an error occurs (other than no rows found), return it
+		return fmt.Errorf("error checking for existing user: %w", err)
+	}
+
+	if err == nil {
+		// If no error and a user was found, return an error for duplicate
+		return fmt.Errorf("user with name '%s' already exists", name)
+	}
+
+	// Proceed with user creation if no existing user was found
+	user, err := s.db.CreateUser(context.Background(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      name,
+	})
+	if err != nil {
+		return fmt.Errorf("couldn't create user: %w", err)
+	}
+
+	// Set the newly created user as the current user
+	err = s.cfg.SetUser(user.Name)
+	if err != nil {
+		return fmt.Errorf("couldn't set current user: %w", err)
+	}
+
+	fmt.Println("User created successfully:")
+	printUser(user)
+	return nil
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %v <name>", cmd.Name)
+	}
+	name := cmd.Args[0]
+
+	_, err := s.db.GetUser(context.Background(), name)
+	if err != nil {
+		return fmt.Errorf("couldn't find user: %w", err)
+	}
+
+	err = s.cfg.SetUser(name)
+	if err != nil {
+		return fmt.Errorf("couldn't set current user: %w", err)
+	}
+
+	fmt.Println("User switched successfully!")
+	return nil
+}
+
+func printUser(user database.User) {
+	fmt.Printf(" * ID:      %v\n", user.ID)
+	fmt.Printf(" * Name:    %v\n", user.Name)
+}
+
+func handlerListUsers(s *state, cmd command)error {
+  users, err := s.db.GetUsers(context.Background())
+  if err != nil {
+    return fmt.Errorf("couldn't list users: :w", err)
+  }
+  for _, user := range users {
+    if user.Name == s.cfg.CurrentUserName {
+      fmt.Printf("* %v (current)\n", user.Name)
+      continue
+    }
+    fmt.Printf("* %v\n", user.Name)
+  }
+  return nil
+}
+
+// handler function begins here
+
+func handlerReset(s *state, cmd command) error {
+	err := s.db.DeleteUsers(context.Background())
+	if err != nil {
+		return fmt.Errorf("couldn't delete users: %w", err)
+	}
+	fmt.Println("Database reset successfully!")
+	return nil
+}
+
+
+// handler agg
+
+func handlerAgg(s *state, cmd command) error {
+  if len(cmd.Args) < 1 || len(cmd.Args) > 2 {
+    return fmt.Errorf("usage: %v <time_between_reqs>", cmd.Name)
+  }
+
+  timeBetweenRequests, err := time.ParseDuration(cmd.Args[0])
+  if err != nil {
+    return fmt.Errorf("invalid duration: %w", err)
+  }
+
+  log.Printf("Collecting feeds every %s...", timeBetweenRequests)
+  ticker := time.NewTicker(timeBetweenRequests)
+
+  for ; ; <-ticker.C {
+    scrapeFeeds(s)
+  }
+}
+
+func scrapeFeeds(s *state) {
+  feed, err := s.db.GetNextFeedToFetch(context.Background())
+  if err != nil {
+    fmt.Println("Couldn't get next feeds to fetch", err)
+    return
+  }
+  log.Println("Found a feed to fetch!")
+  scrapeFeed(s.db, feed)
+}
+
+func scrapeFeed(db *database.Queries, feed database.Feed) {
+  _, err := db.MarkFeedFetched(context.Background(), feed.ID)
+  if err != nil {
+    log.Printf("Couldn't mark feed %s fetched: %v", feed.Name, err)
+    return
+  }
+
+  feedData, err := fetchFeed(context.Background(), feed.Url)
+  if err != nil {
+    log.Printf("Couldn't collect feed %s fetched %v", feed.Name, err)
+    return
+  }
+  for _, item := range feedData.Channel.Item {
+    	  publishedAt := sql.NullTime{}
+	  if t, err := time.Parse(time.RFC1123Z, item.PubDate); err == nil {
+		  publishedAt = sql.NullTime{
+			  Time:  t,
+			  Valid: true,
+		  }
+	  }
+
+	  _, err = db.CreatePost(context.Background(), database.CreatePostParams{
+		  ID:        uuid.New(),
+		  CreatedAt: time.Now().UTC(),
+		  UpdatedAt: time.Now().UTC(),
+		  FeedID:    feed.ID,
+		  Title:     item.Title,
+		  Description: sql.NullString{
+			  String: item.Description,
+			  Valid:  true,
+		  },
+		  Url:         item.Link,
+		  PublishedAt: publishedAt,
+	  })
+	  if err != nil {
+	    if strings.Contains(err.Error(), "duplicate key value violates unique constraint") {
+	      continue
+	  }
+	  log.Printf("Couldn't create post: %v", err)
+	  continue
+	}
+  }
+  log.Printf("Feed %s collected, %v posts found", feed.Name, len(feedData.Channel.Item))
+}
+
+// handler feed
+
+func handlerAddFeed(s *state, cmd command, user database.User) error {
+
+	if len(cmd.Args) != 2 {
+		return fmt.Errorf("usage: %s <name> <url>", cmd.Name)
+	}
+
+	name := cmd.Args[0]
+	url := cmd.Args[1]
+
+	feed, err := s.db.CreateFeed(context.Background(), database.CreateFeedParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		UserID:    user.ID,
+		Name:      name,
+		Url:       url,
+	})
+  if err != nil {
+		return fmt.Errorf("couldn't create feed: %w", err)
+	}
+
+	feedFollow, err := s.db.CreateFeedFollow(context.Background(), database.CreateFeedFollowParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		UserID:    user.ID,
+		FeedID:    feed.ID,
+	})
+	if err != nil {
+		return fmt.Errorf("couldn't create feed follow: %w", err)
+	}
+
+	fmt.Println("Feed created successfully:")
+	printFeed(feed, user)
+	fmt.Println()
+	fmt.Println("Feed followed successfully:")
+	printFeedFollow(feedFollow.UserName, feedFollow.FeedName)
+	fmt.Println("=====================================")
+	return nil
+}
+
+func handlerListFeeds(s *state, cmd command) error {
+	feeds, err := s.db.GetFeeds(context.Background())
+	if err != nil {
+		return fmt.Errorf("couldn't get feeds: %w", err)
+	}
+
+	if len(feeds) == 0 {
+		fmt.Println("No feeds found.")
+		return nil
+	}
+
+	fmt.Printf("Found %d feeds:\n", len(feeds))
+	for _, feed := range feeds {
+		user, err := s.db.GetUserById(context.Background(), feed.UserID)
+		if err != nil {
+			return fmt.Errorf("couldn't get user: %w", err)
+		}
+		printFeed(feed, user)
+		fmt.Println("=====================================")
+	}
+
+	return nil
+}
+
+func printFeed(feed database.Feed, user database.User) {
+	fmt.Printf("* ID:            %s\n", feed.ID)
+	fmt.Printf("* Created:       %v\n", feed.CreatedAt)
+	fmt.Printf("* Updated:       %v\n", feed.UpdatedAt)
+	fmt.Printf("* Name:          %s\n", feed.Name)
+	fmt.Printf("* URL:           %s\n", feed.Url)
+	fmt.Printf("* User:          %s\n", user.Name)
+    fmt.Printf("* LastFetchedAt: %n\n", feed.LastFetchedAt.Time)
+}
+
+// handler follows
+
+func handlerFollow(s *state, cmd command, user database.User) error {
+
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %s <feed_url>", cmd.Name)
+	}
+
+	feed, err := s.db.GetFeedByURL(context.Background(), cmd.Args[0])
+	if err != nil {
+		return fmt.Errorf("couldn't get feed: %w", err)
+	}
+
+	ffRow, err := s.db.CreateFeedFollow(context.Background(), database.CreateFeedFollowParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		UserID:    user.ID,
+		FeedID:    feed.ID,
+	})
+	if err != nil {
+		return fmt.Errorf("couldn't create feed follow: %w", err)
+	}
+
+	fmt.Println("Feed follow created:")
+	printFeedFollow(ffRow.UserName, ffRow.FeedName)
+	return nil
+}
+
+func handlerListFeedFollows(s *state, cmd command, user database.User) error {
+
+	feedFollows, err := s.db.GetFeedFollowsForUser(context.Background(), user.ID)
+	if err != nil {
+		return fmt.Errorf("couldn't get feed follows: %w", err)
+	}
+
+	if len(feedFollows) == 0 {
+		fmt.Println("No feed follows found for this user.")
+		return nil
+	}
+
+	fmt.Printf("Feed follows for user %s:\n", user.Name)
+	for _, ff := range feedFollows {
+		fmt.Printf("* %s\n", ff.FeedName)
+	}
+
+	return nil
+}
+
+func handlerUnfollow(s *state, cmd command, user database.User) error {
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %s <feed_url>", cmd.Name)
+	}
+
+	feed, err := s.db.GetFeedByURL(context.Background(), cmd.Args[0])
+	if err != nil {
+		return fmt.Errorf("couldn't get feed: %w", err)
+	}
+
+	err = s.db.DeleteFeedFollow(context.Background(), database.DeleteFeedFollowParams{
+		UserID: user.ID,
+		FeedID: feed.ID,
+	})
+	if err != nil {
+		return fmt.Errorf("couldn't delete feed follow: %w", err)
+	}
+
+	fmt.Printf("%s unfollowed successfully!\n", feed.Name)
+	return nil
+}
+
+
+// handlerBrowse
+func printFeedFollow(username, feedname string) {
+	fmt.Printf("* User:          %s\n", username)
+	fmt.Printf("* Feed:          %s\n", feedname)
+}
+
+func handlerBrowse(s *state, cmd command, user database.User) error {
+	limit := 2
+	if len(cmd.Args) == 1 {
+		if specifiedLimit, err := strconv.Atoi(cmd.Args[0]); err == nil {
+			limit = specifiedLimit
+		} else {
+			return fmt.Errorf("invalid limit: %w", err)
+		}
+	}
+
+	posts, err := s.db.GetPostsForUser(context.Background(), database.GetPostsForUserParams{
+		UserID: user.ID,
+		Limit:  int32(limit),
+	})
+	if err != nil {
+		return fmt.Errorf("couldn't get posts for user: %w", err)
+	}
+
+	fmt.Printf("Found %d posts for user %s:\n", len(posts), user.Name)
+	for _, post := range posts {
+		fmt.Printf("%s from %s\n", post.PublishedAt.Time.Format("Mon Jan 2"), post.FeedName)
+		fmt.Printf("--- %s ---\n", post.Title)
+		fmt.Printf("    %v\n", post.Description.String)
+		fmt.Printf("Link: %s\n", post.Url)
+		fmt.Println("=====================================")
+	}
+
+	return nil
+}
